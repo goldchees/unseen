339da339ee43efcba87d1a5d1b4c27e4998a7624
goldchees <goldcheese123@gmail.com>
2025-06-18 11:36:14 -0400

    improved rotation, path follow :)

--- scripts/eye.gd
+++ scripts/eye.gd
@@ -5,82 +5,78 @@ extends CharacterBody2D
 @onready var ray_cast_2d: RayCast2D = $RayCast2D
 @onready var pupil: CharacterBody2D = $pupil
 
-@onready var right: Marker2D = $pupilPositions/right
-@onready var down: Marker2D = $pupilPositions/down
-@onready var left: Marker2D = $pupilPositions/left
-@onready var up: Marker2D = $pupilPositions/up
-@onready var center: Marker2D = $pupilPositions/center
-
-#@export var firstMove: states
-#@export var secondMove: states
-#@export var thirdMove: states
-#@export var fourthMove: states
-#@export var fifthMove: states
-#@onready var move_timer: Timer = $moveTimer
-
-#@export var moves: Array[states] = []
-#var num_of_moves: int
-#var move_counter: int = 0
-#var count_dir: String = "forward"
-#var reverse_moves: Array[states] = []
+@onready var path_follow_2d: PathFollow2D = $".."
+# patrol types are linear and loop
+@export_enum("linear", "loop") var patrol_type: String
+# 1 is forward, 0 is backward
+var direction = 1
+# states are patrol and attack
+var current_state: String
 
 var player_inrange
 var player_in_sight
 
-enum states {up, down, right, left, center}
-var current_state: states = states.up
 var pupil_speed := 20
-@export var eyeSpeed := 100
+@export var eyeSpeed := 0.1
 
-#func _ready() -> void:
-	#move_counter = 0
-	#player_inrange = false
-	#num_of_moves = moves.size()
-	#for move in moves:
-		#var r_move: states
-		#match move:
-			#states.up:
-				#r_move = states.down
-				#
-			#states.down:
-				#r_move = states.up
-			#states.right:
-				#r_move = states.left
-			#states.left:
-				#r_move = states.right
-			#states.center:
-				#r_move = states.center
-		#reverse_moves.append(r_move)
-	#move_timer.start()
-	
+
+func _ready() -> void:
+	current_state = "patrol"
+	direction = 1
+
 func _process(delta: float) -> void:
 	if player_inrange:
 		ray_cast_2d.look_at(player.global_position)
 		if ray_cast_2d.is_colliding():
 			var collider = ray_cast_2d.get_collider()
 			if collider.has_method('player'):
-				print('hit player')
-				#current_state = states.up
+				current_state = "attack"
+				# put player death here
+				pass
 
 func _physics_process(delta: float) -> void:
 	match current_state:
-		states.up:
-			#print("up")
-			pupil.global_position = pupil.global_position.move_toward(up.global_position, pupil_speed * delta)
+		"patrol":
+			patrol(delta)
+		"attack":
+			attack()
 
-		states.down:
-			#print("down")
-			pupil.global_position = pupil.global_position.move_toward(down.global_position, pupil_speed * delta)
-		states.right:
-			#print("right")
-			pupil.global_position = pupil.global_position.move_toward(right.global_position, pupil_speed * delta)
-		states.left:
-			#print("left")
-			pupil.global_position = pupil.global_position.move_toward(left.global_position, pupil_speed * delta)
-		states.center:
-			#print("center")
-			pupil.global_position = pupil.global_position.move_toward(center.global_position, pupil_speed * delta)
+func patrol(delta):
+	if patrol_type == "loop":
+		path_follow_2d.progress_ratio += eyeSpeed * delta
+		if rotation_degrees != 0:
+			rotation_degrees = lerp(rotation_degrees, 0.0, 0.2)
+	elif patrol_type == "linear":
+		if direction == 1:
+			if path_follow_2d.progress_ratio >= 0.9:
+				await get_tree().create_timer(0.3).timeout
+				rotation_degrees = lerp(rotation_degrees, 180.0, 0.1)
+				await get_tree().create_timer(1).timeout
+				direction = 0
+			else:
+				path_follow_2d.progress_ratio += eyeSpeed * delta
+				if rotation_degrees != 0:
+					rotation_degrees = lerp(rotation_degrees, 0.0, 0.2)
+		elif direction == 0:
+			if path_follow_2d.progress_ratio <= 0.1:
+				print("hi")
+				await get_tree().create_timer(0.3).timeout
+				rotation_degrees = lerp(rotation_degrees, 0.0, 0.1)
+				await get_tree().create_timer(1).timeout
+				direction = 1
+			else:
+				path_follow_2d.progress_ratio -= eyeSpeed * delta
+				if rotation_degrees != 180:
+					rotation_degrees = lerp(rotation_degrees, 180.0, 0.2)
+		print(path_follow_2d.progress_ratio)
 
+	
+func attack():
+	look_at(player.global_position)
+	if player_inrange == false:
+		current_state = "patrol"
+	
+	
 func _on_vision_area_body_entered(body: Node2D) -> void:
 	if body.has_method('player'):
 		player_inrange = true
@@ -88,59 +84,3 @@ func _on_vision_area_body_entered(body: Node2D) -> void:
 func _on_vision_area_body_exited(body: Node2D) -> void:
 	if body.has_method('player'):
 		player_inrange = false
-
-func set_dir(dir: String):
-	match "right":
-		#current_state = states.right
-		pass
-		
-		
-#ww	# up (0), down (1)
-	#var has_played_forward: bool = false
-	#if count_dir == "forward":
-		#current_state = moves[move_counter]
-		#print(current_state)
-		#print(count_dir)
-		#move_counter += 1
-	#elif count_dir == "reverse":
-		#if move_counter == (num_of_moves - 1) and not has_played_forward:
-			#current_state = moves[move_counter]
-			#has_played_forward = true
-		#elif move_counter == (num_of_moves - 1) and has_played_forward:
-			#current_state = reverse_moves[move_counter]
-			#move_counter -= 1
-		#else:
-			#current_state = reverse_moves[move_counter]
-			#move_counter -= 1
-#
-		#print(current_state)
-		#print(count_dir)
-		#move_counter -= 1
-		#
-	#if move_counter == (num_of_moves - 1):
-		#count_dir = "reverse"
-		#has_played_forward = false
-#
-	#elif move_counter == 0:
-		#count_dir = "forward"
-	
-#	remove
-	#match move_counter:
-		#0:
-			#current_state = firstMove
-			#count_dir = "forward"
-		#1:
-			#current_state = secondMove
-		#2:
-			#current_state = thirdMove
-		#3:
-			#current_state = fourthMove
-		#4:
-			#current_state = fifthMove
-			#count_dir = "reverse"
-
-	#move_timer.start()
-	#
-#
-#func _on_move_timer_timeout() -> void:
-	#move()
--- scripts/path_follow_2d.gd
+++ scripts/path_follow_2d.gd
@@ -1,15 +1,9 @@
 extends PathFollow2D
 @onready var eye: CharacterBody2D = $eye
 
-var speed := 0.1
-
-func _process(delta: float) -> void:
-	var previous_pos = global_position
-	progress_ratio += delta * speed
-	var dir = (global_position - previous_pos).normalized()
-	
-	#if abs(dir.x) > abs(dir.y):
-		#if dir.x > 0:
-			#eye.current_state = states.right
-		#else:
-			#eye.current_state = states.left
+#var speed := 0.1
+#
+#func _process(delta: float) -> void:
+	#var previous_pos = global_position
+	#progress_ratio += delta * speed
+	#var dir = (global_position - previous_pos).normalized()
